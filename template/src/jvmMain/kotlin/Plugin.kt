d2929af6611ccd984bcbebe549462b854040c516
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2023-01-05 20:30:04 +0600

    inited

---
+++ template/src/jvmMain/kotlin/Plugin.kt
@@ -0,0 +1,15 @@
+package dev.inmo.plagubot.suggestionsbot.template
+
+import dev.inmo.micro_utils.fsm.common.State
+import dev.inmo.plagubot.Plugin
+import dev.inmo.tgbotapi.extensions.behaviour_builder.BehaviourContextWithFSM
+import kotlinx.serialization.json.JsonObject
+import org.jetbrains.exposed.sql.Database
+import org.koin.core.Koin
+import org.koin.core.module.Module
+
+object Plugin : Plugin {
+    override fun Module.setupDI(database: Database, params: JsonObject) {}
+
+    override suspend fun BehaviourContextWithFSM<State>.setupBotPlugin(koin: Koin) {}
+}
